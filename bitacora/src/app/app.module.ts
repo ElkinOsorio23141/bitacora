01e70d427e6115f751a8002c52f2b8b4086119d2
ElkinOsorio23141 <elkin_osorio23141@elpoli.edu.co>
2022-10-10 12:34:04 -0500

    se crean los componentes iniciales para la bitacora

--- bitacora/src/app/app.module.ts
+++ bitacora/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Login/login/login.component';
+import { CasosComponent } from './Casos/casos/casos.component';
+import { ClientesComponent } from './Clientes/clientes/clientes.component';
+import { EmpleadosComponent } from './Empleados/empleados/empleados.component';
+import { MenuComponent } from './Menu/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CasosComponent,
+    ClientesComponent,
+    EmpleadosComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
