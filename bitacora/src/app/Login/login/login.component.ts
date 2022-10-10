0eeb29f8fa4f496d5b117313270793f5c01e8e82
Alexisqu20 <68927236+Alexisqu20@users.noreply.github.com>
2022-10-10 15:44:55 -0500

    Agregar navegación

--- bitacora/src/app/Login/login/login.component.ts
+++ bitacora/src/app/Login/login/login.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { PathMap } from 'src/app/@core/enums/path-map.enum';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  clickEvent() {
+    console.log('entro');
+    this.router.navigate([`/${PathMap.Menu}`]);
   }
-
 }
--- bitacora/src/app/app-routing.module.ts
+++ bitacora/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PathMap } from './@core/enums/path-map.enum';
 
 const routes: Routes = [
   {
-    path: '',
+    path: PathMap.Login,
     loadChildren: () =>
       import('./Login/login.module').then((m) => m.LoginModule),
+  },
+  {
+    path: PathMap.Menu,
+    loadChildren: () => import('./Menu/menu.module').then((m) => m.MenuModule),
   },
 ];
 
