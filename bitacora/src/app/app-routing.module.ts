7b43e7e942fe5cae0730c0fbba516fb67dffc207
Alexisqu20 <68927236+Alexisqu20@users.noreply.github.com>
2022-10-10 15:02:07 -0500

    Agregar rutas

--- bitacora/src/app/app-routing.module.ts
+++ bitacora/src/app/app-routing.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-{
-  path: '',
-loadChildren: () => import('./Login/login.module').then(m => m.LoginModule)
-}
+  {
+    path: '',
+    loadChildren: () =>
+      import('./Login/login.module').then((m) => m.LoginModule),
+  },
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes)
-  ],
-  exports: [ RouterModule ],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- bitacora/src/app/app.module.ts
+++ bitacora/src/app/app.module.ts
@@ -1,22 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRoutingModule } from '../app/app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './Login/login/login.component';
-import { CasosComponent } from './Casos/casos/casos.component';
-import { ClientesComponent } from './Clientes/clientes/clientes.component';
-import { EmpleadosComponent } from './Empleados/empleados/empleados.component';
-import { MenuComponent } from './Menu/menu/menu.component';
-import { AppRoutingModule } from '../app/app-routing.module'
 
 @NgModule({
-  declarations: [
-AppComponent
-  ],
-  imports: [
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
